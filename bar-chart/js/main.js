deefeec86879ace70c5673ccddcd166138223cf2
zhangmh <zhangmhao@gmail.com>
2014-04-11 18:25:15 +0800

    长条图 - 消除magic number

--- bar-chart/js/main.js
+++ bar-chart/js/main.js
@@ -2,10 +2,14 @@
     'use strict';
     var d3 = window.d3,
         dataArr = [12, 23, 45, 45, 56, 3, 10];
+
+    var x = d3.scale.linear()
+            .domain([0, d3.max(dataArr)])
+            .range([0, 560]);
     d3.select('.chart')
       .selectAll('div')
       .data(dataArr)
       .enter().append('div')
-      .style('width', function (d) { return d*10 + 'px'; })
+      .style('width', function (d) { return x(d) + 'px'; })
       .text(function (d) { return d; });
 })(window);
