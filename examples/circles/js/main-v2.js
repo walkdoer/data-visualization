1818015c24634fa8291f4eba0c301ff734c6cd6e
zhangmh <zhangmhao@gmail.com>
2014-07-30 11:40:27 +0800

    学习forcelayout

---
+++ examples/circles/js/main-v2.js
@@ -0,0 +1,47 @@
+(function(d3) {
+    'use strict';
+
+
+    var svg = d3.select(".circles");
+
+    var data = [100, 40, 40, 58, 83, 89, 189, 56, 934];
+    var gEl = svg.selectAll('g').data(data)
+        .enter()
+        .append('g')
+        .attr('transform', function(d, i) {
+            return "translate(" + (i * 50 + 30) + ", 60)";
+        });
+    var circles = gEl.append('circle')
+        .attr('r', function(d) {
+            return Math.sqrt(d);
+        })
+        .attr('stroke', 'steelblue')
+        .attr('fill', 'steelblue');
+
+    var getFontSize = function(d) {
+        var fontSize = 30;
+        var max = d3.max(data);
+        var realSize = d * fontSize / max;
+        return realSize < 10 ? 10 : realSize;
+    };
+    gEl.append('text')
+        .text(function(d) {
+            return d;
+        })
+        .attr('font-size', function(d) {
+            return getFontSize(d);
+        })
+        .attr('dx', function() {
+            return -10;
+        })
+        .attr('dy', function(d) {
+            var fontSize = getFontSize(d);
+            return (Math.sqrt(d) - fontSize) / 2;
+        });
+
+    d3.layout.force()
+        .nodes(circles)
+        .size([720, 720])
+        .start();
+
+})(window.d3);
